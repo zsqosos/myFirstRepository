b9d5ac37074a7a30b6541ad82556efd329c0a739
MEMEME <zsqos@hotmail.com>
2016-12-20 21:08:58 +0800

    completed the todo add del edit,not router

--- js/app.js
+++ js/app.js
@@ -8,11 +8,24 @@
 				{ id: 1, text: 'study', completed: false },
 				{ id: 2, text: 'work', completed: true },
 				{ id: 3, text: 'sleep', completed: false }
-			]
+			],
+			currentEditingId: -1,
+			checkedNow: true
 		},
 		methods: {
+			getId: function(){
+				var id = Math.random();
+				for(var i = 0; i < this.todos.length; i++ ){
+					if(this.todos[i].id === id){
+						id = this.getId();
+					}
+				}
+				return id;
+			},
 			add: function(){
-				this.todos.push({ id: 1, text: this.text, completed: false });
+				if(!this.text)
+					return;
+				this.todos.push({ id: this.getId(), text: this.text, completed: false });
 				this.text = '';
 			},
 			remove: function(id){
@@ -21,8 +34,36 @@
 						this.todos.splice(i,1);
 					}
 				}	
+			},
+			clear: function(){
+				var result = [];
+				for(var i = 0; i < this.todos.length; i++){
+					if(!this.todos[i].completed){
+						result.push(this.todos[i]);
+					}
+				}
+				this.todos = result;
+			},
+			existCompleted: function(){
+				for(var i = 0; i < this.todos.length; i++ ){
+					if(this.todos[i].completed){
+						return true;
+					}
+				}
+				return false;
+			},
+			editing: function(id){
+				this.currentEditingId = id;
+			},
+			save: function(){
+				this.currentEditingId = -1;
+			},
+			toggleAll: function(){
+				for(var i = 0; i < this.todos.length; i++){
+					this.todos[i].completed = this.checkedNow;
+				}
+				this.checkedNow = !this.checkedNow;
 			}
 		}
 	})
-
 })(Vue);
